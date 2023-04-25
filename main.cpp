5a7fd477fb7fcae3653f3b554596a0e7a32079f6
Kholopov Pavel <pasha.holopov17@gmail.com>
2023-04-25 09:35:30 +0300

    hello with lastname as well;
    Signed-off-by: Kholopov Pavel <pasha.holopov17@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <string>
 int main()
 {
-std::string name;
+std::string name, lastname;
 std::cout << "Enter your name" << std::endl;
 std::cin >> name;
-std::cout << "Hello, " << name << std::endl;
+std::cout << "Enter your lastname" << std::endl;
+std::cin >> lastname;
+std::cout << "Hello, " << name << lastname << std::endl;
 return 0;
 }
