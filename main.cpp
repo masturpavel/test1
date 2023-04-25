ad664fc870ffa74a2647104c76bc6c3cc8d09058
Kholopov Pavel <pasha.holopov17@gmail.com>
2023-04-25 09:28:32 +0300

    Give a personalized hello

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <string>
 int main()
 {
-std::cout << "Hello World!" << std::endl;
+std::string name;
+std::cout << "Enter your name" << std::endl;
+std::cin >> name;
+std::cout << "Hello, " << name << std::endl;
+return 0;
 }
